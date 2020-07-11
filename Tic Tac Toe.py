f71a330fc8dccfae7dfb01053a1c45064fadc7d7
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-07-11 17:56:34 +0600

    Perfectly Playable!

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -38,62 +38,6 @@ def player_input():
 #marking places on the board based on user input
 def place_marker(board, marker, position):
     board[position] = marker
-<<<<<<< HEAD
-=======
-
-
-#checking if won
-def win_check(board, mark):
-    return(
-        #check rows
-        (board[7] == board[8] == board[9] == mark) or
-        (board[4] == board[5] == board[6] == mark) or
-        (board[1] == board[2] == board[3] == mark) or
-        #check columns
-        (board[3] == board[7] == board[9] == mark) or
-        (board[2] == board[5] == board[8] == mark) or
-        (board[1] == board[4] == board[7] == mark) or
-        #check diagonals
-        (board[3] == board[5] == board[7] == mark) or
-        (board[1] == board[5] == board[9] == mark)    
-     )
-
-
-#choose which player goes first
-def choose_first():
-    
-    flip = random.randint(0,1)
-
-    if (flip == 0):
-        return 'Player 1'
-    else:
-        return 'Player 2'
-
-
-#check if a space is available for marking
-def space_check(board, position):
-   
-    return board[position] == ' '
-
-
-#check if board is full
-def full_board_check(board):
-
-    for (i in range(1, 10)):
-        if space_check(board, i):
-            return False
-    #Board is full if we return true
-    return true
-
-
-#ask for player's next choice
-def player_choice(board):
-
-    position = 0
-
-    while position not in [1,2,3,4,5,6,7,8,] or rnot space_check(board, position):
-
->>>>>>> cbef2af42c0eaab14b2a0bb6c3b3f70b3b2250ef
 
 
 #checking if won
@@ -160,7 +104,7 @@ def new_line():
 #check in players want to play again
 def replay():
 
-    choice = input("Play again? Enter Yes or No : ").lower()
+    choice = input("\nPlay again? Enter Yes or No : ").lower()
 
     return choice == 'yes'
 
@@ -175,7 +119,6 @@ def replay():
 welcome()
 new_line()
 
-<<<<<<< HEAD
 #Beginning of Gameplay
 while True:
 
@@ -266,7 +209,3 @@ while True:
     #break out of the loop on replay
     if not replay():
         break
-=======
-#take player input and initialize player markers by tuple unpacking
-player1_marker, player2_marker = player_input()
->>>>>>> cbef2af42c0eaab14b2a0bb6c3b3f70b3b2250ef
